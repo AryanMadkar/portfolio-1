7d91aeecba380955c3a9c33b21c82b8503ab6a71
Aryan Madkar <164667750+AryanMadkar@users.noreply.github.com>
2024-05-04 23:50:45 +0530

    Delete backend directory

--- backend/index.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config()
-const express = require('express');
-const Port = process.env.PORT || 3000 ;
-const app = express();
-const dbconfig = require("./config/DBconfig")
-
-app.listen(Port,()=>{
-    console.log(`server listening on ${Port}`)
-})
